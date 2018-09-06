6ba57d8470d6a10054f060c91713fed131745d14
Francisco Calaca <chicocx@gmail.com>
2018-09-06 20:01:32 -0300

    todos

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+
+
+clientes = {"Jose": [62981823545, 981234567], "Maria": 12, "Marcos": "da silva"}
+
+print(clientes["Jose"])
+print(clientes["Maria"])
+print(clientes["Marcos"])
+
+req = {"requisicao": 12345678,"pessoas": [{"nome": "Jose da Silva", "telefone": "3232123"},{"nome": "Maria da Silva", "telefone": "3232123"},{"nome": "Carlos", "telefone": "3232123"}]}
+
+print(req["requisicao"])
+
+for p in req["pessoas"]:
+    print(p["nome"])
+
+
+
+pessoas = [{"nome": "Maria","sexo": "F"}, {"nome": "Jose","sexo": "M"}, {"nome": "Carlos","sexo": "M"}, {"nome": "Janaina","sexo": "F"}]
+
+for p in pessoas:
+    print("Prezad{} {}".format("o" if p["sexo"] == "M" else "a", p["nome"]))    
+    
